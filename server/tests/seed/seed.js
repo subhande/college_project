7fe79e53f27ae9fa024cfc7773e9673b755350ed
subhande <subhande123@gmail.com>
2018-02-17 00:06:22 +0530

    Add POST /users and POST /users  test

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,51 @@
+const {ObjectID} = require('mongodb');
+const {User} = require('./../../models/users');
+const jwt = require('jsonwebtoken');
+
+const userOneID = new ObjectID();
+const userTwoID = new ObjectID();
+const userThreeID = new ObjectID();
+const access = 'auth';
+
+
+const users = [{
+    _id: userOneID,
+    email: 'subhan@gmail.com',
+    password: 'useronepass',
+    tokens:[{
+        access: 'auth',
+        token: jwt.sign({_id: userOneID,access},process.env.JWT_SECRET).toString()
+    }]
+
+},{
+    _id: userTwoID,
+    email: 'subhan1@gmail.com',
+    password: 'usertwopass',
+    tokens:[{
+        access: 'auth',
+        token: jwt.sign({_id: userTwoID,access},process.env.JWT_SECRET).toString()
+    }]
+
+},{
+    _id: userThreeID,
+    email: 'subhan12@gmail.com',
+    password: 'useronepass',
+    tokens:[{
+        access: 'auth',
+        token: jwt.sign({_id: userThreeID,access},process.env.JWT_SECRET).toString()
+    }]
+
+}];
+
+const populateUser = async (done) =>{
+    try {
+        let userOne = await new User(users[0]).save();
+        let userTwo = await new User(users[1]).save();
+        let userThree = await new User(users[2]).save();
+        done();
+    } catch(e) {
+        done(e);
+    }
+};
+
+module.exports = { users, populateUser };
---
+++ server/tests/server.test.js
@@ -0,0 +1,36 @@
+const expect = require('expect');
+const request = require('supertest');
+const {ObjectID} = require('mongodb');
+
+const {app} = require('./../server');
+const {User} = require('./../models/users');
+
+const {users, populateUser} = require('./seed/seed');
+
+
+
+//beforeEach(populateUser);
+
+
+describe('POST /users', () => {
+    it('should create a user',(done) => {
+        let email = 'subh@gmail.com';
+        let password = 'abc123';
+
+        request(app)
+            .post('/users')
+            .send({email,password})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBeTruthy();
+                expect(res.body.email).toBeTruthy();
+            })
+            .end((err) => {
+                if(err) {
+                    return done(err);
+                }
+                done();
+            });
+
+    });
+});
